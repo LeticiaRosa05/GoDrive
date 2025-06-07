e7e8193076b9a9219993b155906d3b763bec125a
Letícia Rosa <leticiasxrosa@gmail.com>
2025-06-06 23:21:31 -0300

    Finalização do desenvolvimento da página home

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ imagens.forEach(img => {
     img.style.transition = "transform 0.2s ease"; //deixa a transição de tamanhos suave
 
     img.addEventListener('mouseover', () => {
-        img.style.transform = "scale(1.15)";
+        img.style.transform = "scale(1.2)";
     });
 
     img.addEventListener('mouseout', () => {
