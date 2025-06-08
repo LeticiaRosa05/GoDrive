27b359ae4ee69dc2c69f23ec2949c64baad89feb
Letícia Rosa <leticiasxrosa@gmail.com>
2025-06-08 12:06:32 -0300

    Adição de querySelectors para novos efeitos; atribuição de cores conforme aparição dos campos do formulário; desenvolvimento do efeito de mudança de cor, escala e sombra do botão "Enviar" do formulário

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ window.addEventListener('scroll', function() {
 
     const mudaCor = document.querySelector('.mudaCor');
     const sectionMs = document.querySelectorAll('.section_modal');
+    const campoM = document.querySelectorAll('.modal_campo');
     const transp = document.querySelectorAll('.transparente');
     const borderS = document.querySelectorAll('.borderStyle');
+    const borderCSS = document.querySelectorAll('.borderStyleCSS');
     const h1s = document.querySelectorAll('h1');
     const ps = document.querySelectorAll('p');
 
@@ -16,10 +18,12 @@ window.addEventListener('scroll', function() {
         const mudaCorTop = mudaCor.getBoundingClientRect().top;
         if (mudaCorTop <= 0) {
             sectionMs.forEach(sectionM => sectionM.style.backgroundColor = '#2c2c2c');
+            campoM.forEach(campoM => campoM.style.backgroundColor = '#222222');
             h1s.forEach(h1 => h1.style.color = '#e9e7e7');
             ps.forEach(p => p.style.color = '#e9e7e7');
         } else {
             sectionMs.forEach(sectionM => sectionM.style.backgroundColor = '#f3f3f3');
+            campoM.forEach(campoM => campoM.style.backgroundColor = '#e8e7e7');
             h1s.forEach(h1 => h1.style.color = '#2c2c2c');
             ps.forEach(p => p.style.color = '#2c2c2c');
         }
@@ -29,7 +33,26 @@ window.addEventListener('scroll', function() {
         const transpTop = transp[0].getBoundingClientRect().top;
         if (transpTop <= 500) {
             transp.forEach(t => t.style.backgroundColor = 'rgba(44, 44, 44, 0.88)');
+            campoM.forEach(campoM => campoM.style.backgroundColor = 'rgba(34, 34, 34, 0.95)');
             borderS.forEach(t => t.style.borderColor = '#096300');
+            borderCSS.forEach(bCSS => {
+                bCSS.style.backgroundColor = '#222222';
+                bCSS.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.5)';
+                bCSS.addEventListener('mouseover', () => {
+                    bCSS.style.backgroundColor = "#17d80544";
+                });
+                bCSS.addEventListener('mouseout', () => {
+                    bCSS.style.backgroundColor = "#222222";
+                });
+                bCSS.addEventListener('click', () => {
+                    bCSS.style.transform = "scale(0.80)";
+                    bCSS.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.2)';
+                    setTimeout(() => {
+                        bCSS.style.transform = "scale(1)";
+                        bCSS.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.5)';
+                    }, 110); /* volta ao normal em 110ms */
+                });
+            });
             h1s.forEach(h1 => h1.style.color = '#e9e7e7');
             ps.forEach(p => p.style.color = '#e9e7e7');
         } else {
