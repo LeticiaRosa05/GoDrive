baaeb1283ffbf81a67331a359f30b41cddbf2a9f
Letícia Rosa <leticiasxrosa@gmail.com>
2025-06-11 21:49:21 -0300

    Estilização do paralax da pg Serviços + ação de scroll

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 window.addEventListener('scroll', function() {
     let parallax = document.querySelector('.GDH_paralax');
     let nF_parallax = document.querySelector('.GDnF_paralax');
+    let S_parallax = document.querySelector('.GDS_paralax');
     let scrollPosition = window.pageYOffset;
     if (parallax) {
         parallax.style.backgroundPositionY = -(scrollPosition * 0.5) + 'px';
     }
     if (nF_parallax) {
         nF_parallax.style.backgroundPositionY = -(scrollPosition * 0.5) + 'px';
+    }
+    if (S_parallax) {
+        S_parallax.style.backgroundPositionY = -(scrollPosition * 0.5) + 'px';
     }
 
     const mudaCor = document.querySelector('.mudaCor');
